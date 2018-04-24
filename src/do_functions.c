1149ef980f2744009a0e6cb1da627b67bcc7b1c0
Podgorny98 <podgorny98@mail.ru>
2018-04-24 11:31:33 +0300

    Added do_functions: AND, ADD, SUB, CP, MOV, PUSH, POP

--- src/do_functions.c
+++ src/do_functions.c
@@ -37,33 +37,106 @@ DO_FUNC(CBI,
 DO_FUNC(SBIS,
 {
 })
-
+#define Rd chip->registers[instr->args.arg[1]]
+#define Rr chip->registers[instr->args.arg[0]]
 DO_FUNC(AND,
 {
-})
+    Rd = Rd & Rr;
 
+    if(Rd == 0)
+        chip->SREG |= _BV(SREG_Z);
+    else
+        chip->SREG &= _BV(SREG_Z);
+
+    if(Rd < 0) {                        // S = N ^ V, V = 0 => S = N
+        chip->SREG |= _BV(SREG_N);
+        chip->SREG |= _BV(SREG_S);
+    } else {
+        chip->SREG &= _BV(SREG_N);
+        chip->SREG &= _BV(SREG_S);
+    }
+
+    chip->SREG &= _BV(SREG_V);
+
+
+})
 DO_FUNC(ADD,
 {
+    int Rd7 = (Rd >> 7);
+    int Rr7 = (Rr >> 7);
+    Rd = Rd + Rr;
+    int R7 = (Rd >> 7);
+    int SREG_bits[5] = {0};
+    SREG_bits[SREG_C] = (Rd7 & Rr7) | (Rr7 & ~R7) | (~R7 & Rd7);
+    SREG_bits[SREG_Z] = !Rd;
+    SREG_bits[SREG_N] = R7;
+    SREG_bits[SREG_V] = (Rd7 & Rr7 & ~R7) | (~Rd7 & ~Rr7 & R7);
+    SREG_bits[SREG_S] = SREG_bits[SREG_N] ^ SREG_bits[SREG_V];
+    for (int i = 0; i < 5; i++) {
+        if (SREG_bits[i])
+            chip->SREG |= _BV(i);
+        else
+            chip->SREG &= ~_BV(i);
+    }
 })
 
 DO_FUNC(SUB,
 {
+    int Rd7 = (Rd >> 7);
+    int Rr7 = (Rr >> 7);
+    Rd = Rd - Rr;
+    int R7 = (Rd >> 7);
+    int SREG_bits[5] = {0};
+    SREG_bits[SREG_C] = (~Rd7 & Rr7) | (Rr7 & R7) | (R7 & ~Rd7);
+    SREG_bits[SREG_Z] = !Rd;
+    SREG_bits[SREG_N] = R7;
+    SREG_bits[SREG_V] = (Rd7 & ~Rr7 & ~R7) | (~Rd7 & Rr7 & R7);
+    SREG_bits[SREG_S] = SREG_bits[SREG_N] ^ SREG_bits[SREG_V];
+    for (int i = 0; i < 5; i++) {
+        if (SREG_bits[i])
+            chip->SREG |= _BV(i);
+        else
+            chip->SREG &= ~_BV(i);
+    }
 })
 
 DO_FUNC(CP,
 {
+    int Rd7 = (Rd >> 7);
+    int Rr7 = (Rr >> 7);
+    int R7 = ((Rd - Rr) >> 7);
+    int SREG_bits[5] = {0};
+    SREG_bits[SREG_C] = (~Rd7 & Rr7) | (Rr7 & R7) | (R7 & ~Rd7);
+    SREG_bits[SREG_Z] = !(Rd - Rr);
+    SREG_bits[SREG_N] = R7;
+    SREG_bits[SREG_V] = (Rd7 & ~Rr7 & ~R7) | (~Rd7 & Rr7 & R7);
+    SREG_bits[SREG_S] = SREG_bits[SREG_N] ^ SREG_bits[SREG_V];
+    for (int i = 0; i < 5; i++) {
+        if (SREG_bits[i])
+            chip->SREG |= _BV(i);
+        else
+            chip->SREG &= ~_BV(i);
+    }
+
 })
 
 DO_FUNC(MOV,
 {
+    Rd = Rr;
 })
 
 DO_FUNC(PUSH,
 {
+    if(chip->SPL <= (REGISTERS_NUM + IO_REGISTERS_NUM))
+        return ERR_STACK_OVERFLOW;
+    chip->data_memory[chip->SPL--] = Rr;
 })
 
 DO_FUNC(POP,
 {
+    if(chip->SPL == DATA_MEMORY_SIZE - 1)
+       return ERR_EMPTY_STACK;
+    Rr = chip->data_memory[(chip->SPL)++];
 })
 
 DO_FUNC(BREQ,
@@ -100,3 +173,5 @@ DO_FUNC(RETI,
 })
 
 #undef DO_FUNC
+#undef Rd
+#undef Rr
