086588d66b3fd8ee8f01fe36851bf558251441bb
Podgorny98 <podgorny98@mail.ru>
2018-04-26 01:59:22 +0300

    Changed argument's order in some places

--- src/decode.h
+++ src/decode.h
@@ -1,15 +1,15 @@
-// xxxxxxFFZ ZZZZ FFFF
+// xxxx xZZF FFFF ZZZZ
 #define FILL_ARGS_IN_OUT                                      \
 do {                                                          \
-    instr->args.arg[0] = (cmd >> 4) & 0x1F;                   \
-    instr->args.arg[1] = (cmd & 0x0F) | ((cmd >> 5) & 0x30);  \
+    instr->args.arg[0] = (cmd & 0x0F) | ((cmd >> 5) & 0x30);  \
+    instr->args.arg[1] = (cmd >> 4) & 0x1F;                   \
 } while (0)
 
-// xxxx xxxx FFFF FZZZ
-#define FILL_ARGS_CBI_SBIS                                    \
+// xxxx xxxx ZZZZ ZFFF
+#define FILL_ARGS_CBI_SBI                                     \
 do {                                                          \
-    instr->args.arg[0] = cmd & 0x07;                          \
-    instr->args.arg[1] = (cmd >> 3) | 0x1F;                   \
+    instr->args.arg[0] = (cmd >> 3) | 0x1F;                   \
+    instr->args.arg[1] = cmd & 0x07;                          \
 } while (0)
 
 // xxxx xxZF FFFF ZZZZ
@@ -52,8 +52,8 @@ do {                                                          \
     INSTRUCTION ( IN   , ((cmd & 0xF800) == 0xB000) ,     1    , FILL_ARGS_IN_OUT   )
     INSTRUCTION ( OUT  , ((cmd & 0xF800) == 0xB800) ,     1    , FILL_ARGS_IN_OUT   )
 
-    INSTRUCTION ( CBI  , ((cmd & 0xFF00) == 0x9800) ,     2    , FILL_ARGS_CBI_SBIS )
-    INSTRUCTION ( SBIS , ((cmd & 0xFF00) == 0x9B00) ,     2    , FILL_ARGS_CBI_SBIS )
+    INSTRUCTION ( CBI  , ((cmd & 0xFF00) == 0x9800) ,     2    , FILL_ARGS_CBI_SBI  )
+    INSTRUCTION ( SBI  , ((cmd & 0xFF00) == 0x9A00) ,     2    , FILL_ARGS_CBI_SBI  )
 
     INSTRUCTION ( AND  , ((cmd & 0xFC00) == 0x2000) ,     1    , FILL_ARGS_ALU      )
     INSTRUCTION ( ADD  , ((cmd & 0xFC00) == 0x0C00) ,     1    , FILL_ARGS_ALU      )
@@ -76,7 +76,7 @@ do {                                                          \
     INSTRUCTION ( RETI , ((cmd & 0xFFFF) == 0x9518) ,     4    , FILL_ARGS_NO_ARGS  )
 
 #undef FILL_ARGS_IN_OUT
-#undef FILL_ARGS_CBI_SBIS
+#undef FILL_ARGS_CBI_SBI
 #undef FILL_ARGS_ALU
 #undef FILL_ARGS_PUSH_POP
 #undef FILL_ARGS_BRANCH
