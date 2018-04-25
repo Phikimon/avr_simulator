6b4914404447383a41acfb18241e42bdf33a9c43
Podgorny98 <podgorny98@mail.ru>
2018-04-26 02:16:33 +0300

    Added IN, OUT, CBI, SBI, BREQ, BRNE

--- src/do_functions.c
+++ src/do_functions.c
@@ -1,4 +1,10 @@
 #include "attiny13.h"
+#include "assert.h"
+
+
+int is_reserved(int8_t offset);
+
+
 
 #define DO_FUNC(INSTR_NAME, CODE)                   \
 int do_##INSTR_NAME(struct attiny13* chip,          \
@@ -6,7 +12,7 @@ int do_##INSTR_NAME(struct attiny13* chip,          \
 {                                                   \
     instr->progress++;                              \
     CODE;                                           \
-    if (instr->progress == INSTR_NAME##_DURATION)   \
+    if (instr->progress == instr->duration)         \
         instr->progress = 0;                        \
     return ERR_SUCCESS;                             \
 }
@@ -19,33 +25,74 @@ DO_FUNC(HANDLE_INTERRUPT,
         chip->PC = instr->args.arg[0];      // Jump to interrupt vector
 })
 
+int is_reserved(int8_t offset) {
+#define IO_REGISTER(NAME, OFFSET)   \
+    case OFFSET:                    \
+        return 0;
+#define RESERVED_REGISTER(OFFSET)   \
+    case OFFSET:                    \
+        return 1;
+    switch (offset) {
+#include "registers_list.h"
+#undef IO_REGISTER
+#undef RESERVED_REGISTER
+        default:
+            assert(!"Wrong xyuo-mayo");
+    }
+}
+
+#define __Rd chip->registers[instr->args.arg[1]]
+#define __Rr chip->registers[instr->args.arg[0]]
+#define __A  instr->args.arg[0]
+#define __b  instr->args.arg[1]
+#define __k  instr->args.addr
+
 DO_FUNC(IN,
 {
+    if (is_reserved(__A))
+        __Rd = 0;
+    else
+        __Rd = chip->io_registers[__A];
+    chip->PC++;
 })
 
 DO_FUNC(OUT,
 {
+    if (!is_reserved(__A))
+        chip->io_registers[__A] = __Rd;
+    chip->PC++;
 })
+
 
 DO_FUNC(CBI,
 {
+    if (instr->progress == 1)
+        return ERR_SUCCESS;
+    if (!is_reserved(__A))
+        chip->io_registers[__A] &= ~_BV(__b);
+    chip->PC++;
 })
 
-DO_FUNC(SBIS,
+DO_FUNC(SBI,
 {
+    if (instr->progress == 1)
+        return ERR_SUCCESS;
+    if (!is_reserved(__A))
+        chip->io_registers[__A] |= _BV(__b);
+    chip->PC++;
 })
-#define Rd chip->registers[instr->args.arg[1]]
-#define Rr chip->registers[instr->args.arg[0]]
+
+
 DO_FUNC(AND,
 {
-    Rd = Rd & Rr;
+    __Rd = __Rd & __Rr;
 
-    if(Rd == 0)
+    if (__Rd == 0)
         chip->SREG |= _BV(SREG_Z);
     else
         chip->SREG &= _BV(SREG_Z);
 
-    if(Rd < 0) {                        // S = N ^ V, V = 0 => S = N
+    if (__Rd < 0) {                        // S = N ^ V, V = 0 => S = N
         chip->SREG |= _BV(SREG_N);
         chip->SREG |= _BV(SREG_S);
     } else {
@@ -59,13 +106,13 @@ DO_FUNC(AND,
 })
 DO_FUNC(ADD,
 {
-    int Rd7 = (Rd >> 7);
-    int Rr7 = (Rr >> 7);
-    Rd = Rd + Rr;
-    int R7 = (Rd >> 7);
+    int Rd7 = (__Rd >> 7);
+    int Rr7 = (__Rr >> 7);
+    __Rd = __Rd + __Rr;
+    int R7 = (__Rd >> 7);
     int SREG_bits[5] = {0};
     SREG_bits[SREG_C] = (Rd7 & Rr7) | (Rr7 & ~R7) | (~R7 & Rd7);
-    SREG_bits[SREG_Z] = !Rd;
+    SREG_bits[SREG_Z] = !__Rd;
     SREG_bits[SREG_N] = R7;
     SREG_bits[SREG_V] = (Rd7 & Rr7 & ~R7) | (~Rd7 & ~Rr7 & R7);
     SREG_bits[SREG_S] = SREG_bits[SREG_N] ^ SREG_bits[SREG_V];
@@ -80,13 +127,13 @@ DO_FUNC(ADD,
 
 DO_FUNC(SUB,
 {
-    int Rd7 = (Rd >> 7);
-    int Rr7 = (Rr >> 7);
-    Rd = Rd - Rr;
-    int R7 = (Rd >> 7);
+    int Rd7 = (__Rd >> 7);
+    int Rr7 = (__Rr >> 7);
+    __Rd = __Rd - __Rr;
+    int R7 = (__Rd >> 7);
     int SREG_bits[5] = {0};
     SREG_bits[SREG_C] = (~Rd7 & Rr7) | (Rr7 & R7) | (R7 & ~Rd7);
-    SREG_bits[SREG_Z] = !Rd;
+    SREG_bits[SREG_Z] = !__Rd;
     SREG_bits[SREG_N] = R7;
     SREG_bits[SREG_V] = (Rd7 & ~Rr7 & ~R7) | (~Rd7 & Rr7 & R7);
     SREG_bits[SREG_S] = SREG_bits[SREG_N] ^ SREG_bits[SREG_V];
@@ -101,12 +148,12 @@ DO_FUNC(SUB,
 
 DO_FUNC(CP,
 {
-    int Rd7 = (Rd >> 7);
-    int Rr7 = (Rr >> 7);
-    int R7 = ((Rd - Rr) >> 7);
+    int Rd7 = (__Rd >> 7);
+    int Rr7 = (__Rr >> 7);
+    int R7 = ((__Rd - __Rr) >> 7);
     int SREG_bits[5] = {0};
     SREG_bits[SREG_C] = (~Rd7 & Rr7) | (Rr7 & R7) | (R7 & ~Rd7);
-    SREG_bits[SREG_Z] = !(Rd - Rr);
+    SREG_bits[SREG_Z] = !(__Rd - __Rr);
     SREG_bits[SREG_N] = R7;
     SREG_bits[SREG_V] = (Rd7 & ~Rr7 & ~R7) | (~Rd7 & Rr7 & R7);
     SREG_bits[SREG_S] = SREG_bits[SREG_N] ^ SREG_bits[SREG_V];
@@ -121,40 +168,56 @@ DO_FUNC(CP,
 
 DO_FUNC(MOV,
 {
-    Rd = Rr;
+    __Rd = __Rr;
     chip->PC++;
 })
 
 DO_FUNC(PUSH,
 {
-    if(instr->progress == 1)
+    if (instr->progress == 1)
         return ERR_SUCCESS;
-    if(chip->SPL <= (REGISTERS_NUM + IO_REGISTERS_NUM))
+    if (chip->SPL <= (REGISTERS_NUM + IO_REGISTERS_NUM))
         return ERR_STACK_OVERFLOW;
-    chip->data_memory[chip->SPL--] = Rr;
+    chip->data_memory[chip->SPL--] = __Rr;
     chip->PC++;
 })
 
 DO_FUNC(POP,
 {
-    if(instr->progress == 1)
+    if (instr->progress == 1)
         return ERR_SUCCESS;
-    if(chip->SPL == DATA_MEMORY_SIZE - 1)
+    if (chip->SPL == DATA_MEMORY_SIZE - 1)
        return ERR_EMPTY_STACK;
-    Rr = chip->data_memory[(chip->SPL)++];
+    __Rr = chip->data_memory[(chip->SPL)++];
     chip->PC++;
 })
 
+#define DO_CONDITIONAL_BRANCH(CONDITION)                        \
+    do {                                                        \
+        if (instr->progress == 1 && !(CONDITION)) {             \
+            instr->duration = 1;    /* No branch */             \
+            chip->PC++;                                         \
+            return ERR_SUCCESS;                                 \
+        }                                                       \
+        chip->PC = (chip->PC + __k + 1) % FLASH_MEMORY_SIZE;    \
+        return ERR_SUCCESS;                                     \
+    } while (0)
+
+
+
 DO_FUNC(BREQ,
 {
+    DO_CONDITIONAL_BRANCH(GET_FLAG_Z);
 })
 
 DO_FUNC(BRNE,
 {
+    DO_CONDITIONAL_BRANCH(GET_FLAG_Z == 0);
 })
 
 DO_FUNC(BRGE,
 {
+    DO_CONDITIONAL_BRANCH((GET_FLAG_N ^ GET_FLAG_V) == 0);
 })
 
 DO_FUNC(CPI,
@@ -179,5 +242,9 @@ DO_FUNC(RETI,
 })
 
 #undef DO_FUNC
-#undef Rd
-#undef Rr
+#undef __Rd
+#undef __Rr
+#undef __A
+#undef __b
+#undef __k
+
