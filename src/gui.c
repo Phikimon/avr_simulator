070d62ee01050370f3e02086b1ea730e43d6e98d
Philippe Mikoyan <philippe.mikoyan@skat.systems>
2018-05-02 23:36:30 +0300

    Update gui files
    Signed-off-by: Philippe Mikoyan <philippe.mikoyan@skat.systems>

--- src/gui.c
+++ src/gui.c
@@ -1,25 +1,22 @@
 #include "gui.h"
+#include "common.h"
+#include <errno.h>
+#include <assert.h>
 
-void configure_gui(int argc, char *argv[])
+GtkWidget* gui_configure(int argc, char *argv[])
 {
     gtk_init(&argc, &argv);
 
-    GtkWidget* window = create_window();
+    GtkWidget* window = gui_create_window();
     gtk_widget_show (window);
 
-    GtkWidget* PB3_LED = find_child(window, "F_PB3_LED");
-    if (!PB3_LED)
-    {
-        g_critical("Cannot get PB3_LED widget");
-    }
-
-    gtk_image_set_from_icon_name((GtkImage*)PB3_LED, "go-bottom", GTK_ICON_SIZE_BUTTON);
-
-    gtk_main();
+    return window;
 }
 
-GtkWidget* find_child(GtkWidget* parent, const gchar* name)
+GtkWidget* gui_find_widget_child(GtkWidget* parent, const gchar* name)
 {
+    assert(parent);
+    assert(name);
     const gchar* parent_name = gtk_widget_get_name((GtkWidget*)parent);
     if (!g_ascii_strcasecmp(parent_name, name))
     {
@@ -28,13 +25,13 @@ GtkWidget* find_child(GtkWidget* parent, const gchar* name)
 
     if (GTK_IS_BIN(parent)) {
         GtkWidget *child = gtk_bin_get_child(GTK_BIN(parent));
-        return find_child(child, name);
+        return gui_find_widget_child(child, name);
     }
 
     if (GTK_IS_CONTAINER(parent)) {
         GList *children = gtk_container_get_children(GTK_CONTAINER(parent));
         do {
-            GtkWidget* widget = find_child(children->data, name);
+            GtkWidget* widget = gui_find_widget_child(children->data, name);
             if (widget != NULL) {
                 return widget;
             }
@@ -44,7 +41,28 @@ GtkWidget* find_child(GtkWidget* parent, const gchar* name)
     return NULL;
 }
 
-GtkWidget* create_window(void)
+void
+__attribute__((format(printf,2,3)))
+printf_gui(GtkWindow* window, const char* format, ...)
+{
+    char buf[MAX_STR_LEN] = {'\0'};
+
+    va_list args;
+    va_start(args,format);
+    vsprintf(buf, format, args);
+    va_end(args);
+
+    GtkDialogFlags flags = GTK_DIALOG_DESTROY_WITH_PARENT;
+    GtkWidget* dialog = gtk_message_dialog_new (GTK_WINDOW(window),
+                                     flags,
+                                     GTK_MESSAGE_ERROR,
+                                     GTK_BUTTONS_CLOSE,
+                                     buf);
+    gtk_dialog_run (GTK_DIALOG (dialog));
+    gtk_widget_destroy (dialog);
+}
+
+GtkWidget* gui_create_window(void)
 {
     GError* error = NULL;
 
--- src/gui.h
+++ src/gui.h
@@ -3,10 +3,13 @@
 
 #include <gtk/gtk.h>
 
-GtkWidget* create_window(void);
+// Public
+GtkWidget* gui_configure(int argc, char *argv[]);
 
-GtkWidget* find_child   (GtkWidget* parent, const gchar* name);
-
-void configure_gui(int argc, char *argv[]);
+//Private
+GtkWidget* gui_create_window    (void);
+GtkWidget* gui_find_widget_child(GtkWidget* parent, const gchar* name);
+void printf_gui(GtkWindow* window, const char* format, ...)
+__attribute__((format(printf,2,3)));
 
 #endif
