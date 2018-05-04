c69a12a4afd7eba95174fd40fd5749df10efaf67
Podgorny98 <podgorny98@mail.ru>
2018-05-05 01:34:25 +0300

    Changed defines names

--- src/decode.h
+++ src/decode.h
@@ -20,7 +20,7 @@ do {                                                             \
 } while (0)
 
 // xxxx xxxd dddd xxxx
-#define FILL_ARGS_RD_ONLY                                        \
+#define FILL_ARGS_Rd_ONLY                                        \
 do {                                                             \
     chip->cmd.args.arg[1] = (cmd >> 4) & 0x1F;                   \
 } while (0)
@@ -38,14 +38,14 @@ do {                                                             \
 } while (0)
 
 // xxxx KKKK dddd KKKK
-#define FILL_ARGS_LDI_CPI                                        \
+#define FILL_ARGS_Rd_K                                           \
 do {                                                             \
     chip->cmd.args.arg[0] = (cmd & 0x0F) | ((cmd >> 4) & 0xF0);  \
     chip->cmd.args.arg[1] = ((cmd >> 4) & 0x0F) + 16;            \
 } while (0)
 
 // xxxx xxdd dddd dddd
-#define FILL_ARGS_LONG_D                                         \
+#define FILL_ARGS_LONG_Rd                                        \
 do {                                                             \
     chip->cmd.args.arg[1] = cmd & 0x001F;                        \
 } while (0)
@@ -69,31 +69,31 @@ do {                                                             \
 
     INSTRUCTION ( AND   , ((cmd & 0xFC00) == 0x2000) ,     1    , FILL_ARGS_ALU        )
     INSTRUCTION ( OR    , ((cmd & 0xFC00) == 0x2800) ,     1    , FILL_ARGS_ALU        )
-    INSTRUCTION ( NEG   , ((cmd & 0xFC00) == 0x9401) ,     1    , FILL_ARGS_RD_ONLY    )
+    INSTRUCTION ( NEG   , ((cmd & 0xFC00) == 0x9401) ,     1    , FILL_ARGS_Rd_ONLY    )
     INSTRUCTION ( ADD   , ((cmd & 0xFC00) == 0x0C00) ,     1    , FILL_ARGS_ALU        )
     INSTRUCTION ( SUB   , ((cmd & 0xFC00) == 0x1800) ,     1    , FILL_ARGS_ALU        )
-    INSTRUCTION ( DEC   , ((cmd & 0xFC00) == 0x940A) ,     1    , FILL_ARGS_RD_ONLY    )
-    INSTRUCTION ( INC   , ((cmd & 0xFC00) == 0x9403) ,     1    , FILL_ARGS_RD_ONLY    )
+    INSTRUCTION ( DEC   , ((cmd & 0xFC00) == 0x940A) ,     1    , FILL_ARGS_Rd_ONLY    )
+    INSTRUCTION ( INC   , ((cmd & 0xFC00) == 0x9403) ,     1    , FILL_ARGS_Rd_ONLY    )
     INSTRUCTION ( CP    , ((cmd & 0xFC00) == 0x1400) ,     1    , FILL_ARGS_ALU        )
     INSTRUCTION ( MOV   , ((cmd & 0xFC00) == 0x2C00) ,     1    , FILL_ARGS_ALU        )
-    INSTRUCTION ( CLR   , ((cmd & 0xFC00) == 0x2400) ,     1    , FILL_ARGS_LONG_D     )
+    INSTRUCTION ( CLR   , ((cmd & 0xFC00) == 0x2400) ,     1    , FILL_ARGS_LONG_Rd    )
     INSTRUCTION ( SER   , ((cmd & 0xFF0F) == 0xEF0F) ,     1    , FILL_ARGS_SER        )
-    INSTRUCTION ( TST   , ((cmd & 0xFC00) == 0x2000) ,     1    , FILL_ARGS_LONG_D     )
-    INSTRUCTION ( LSR   , ((cmd & 0xFE0F) == 0x9406) ,     1    , FILL_ARGS_RD_ONLY    )
-    INSTRUCTION ( LSL   , ((cmd & 0xFC00) == 0x0C00) ,     1    , FILL_ARGS_LONG_D     )
+    INSTRUCTION ( TST   , ((cmd & 0xFC00) == 0x2000) ,     1    , FILL_ARGS_LONG_Rd    )
+    INSTRUCTION ( LSR   , ((cmd & 0xFE0F) == 0x9406) ,     1    , FILL_ARGS_Rd_ONLY    )
+    INSTRUCTION ( LSL   , ((cmd & 0xFC00) == 0x0C00) ,     1    , FILL_ARGS_LONG_Rd    )
 
-    INSTRUCTION ( PUSH  , ((cmd & 0xFE0F) == 0x920F) ,     2    , FILL_ARGS_RD_ONLY    )
-    INSTRUCTION ( POP   , ((cmd & 0xFE0F) == 0x900F) ,     2    , FILL_ARGS_RD_ONLY    )
+    INSTRUCTION ( PUSH  , ((cmd & 0xFE0F) == 0x920F) ,     2    , FILL_ARGS_Rd_ONLY    )
+    INSTRUCTION ( POP   , ((cmd & 0xFE0F) == 0x900F) ,     2    , FILL_ARGS_Rd_ONLY    )
 
-    INSTRUCTION ( LD_X  , ((cmd & 0xFE0F) == 0x900C) ,     1    , FILL_ARGS_RD_ONLY    )
-    INSTRUCTION ( LD_Y  , ((cmd & 0xFE0F) == 0x8008) ,     1    , FILL_ARGS_RD_ONLY    )
-    INSTRUCTION ( LD_Z  , ((cmd & 0xFE0F) == 0x8000) ,     1    , FILL_ARGS_RD_ONLY    )
+    INSTRUCTION ( LD_X  , ((cmd & 0xFE0F) == 0x900C) ,     1    , FILL_ARGS_Rd_ONLY    )
+    INSTRUCTION ( LD_Y  , ((cmd & 0xFE0F) == 0x8008) ,     1    , FILL_ARGS_Rd_ONLY    )
+    INSTRUCTION ( LD_Z  , ((cmd & 0xFE0F) == 0x8000) ,     1    , FILL_ARGS_Rd_ONLY    )
 
-    INSTRUCTION ( ST_X  , ((cmd & 0xFE0F) == 0x920C) ,     2    , FILL_ARGS_RD_ONLY    )
-    INSTRUCTION ( ST_Y  , ((cmd & 0xFE0F) == 0x8208) ,     2    , FILL_ARGS_RD_ONLY    )
-    INSTRUCTION ( ST_Z  , ((cmd & 0xFE0F) == 0x8200) ,     2    , FILL_ARGS_RD_ONLY    )
+    INSTRUCTION ( ST_X  , ((cmd & 0xFE0F) == 0x920C) ,     2    , FILL_ARGS_Rd_ONLY    )
+    INSTRUCTION ( ST_Y  , ((cmd & 0xFE0F) == 0x8208) ,     2    , FILL_ARGS_Rd_ONLY    )
+    INSTRUCTION ( ST_Z  , ((cmd & 0xFE0F) == 0x8200) ,     2    , FILL_ARGS_Rd_ONLY    )
 
-    INSTRUCTION ( LPM   , ((cmd & 0xFE0F) == 0x9004) ,     3    , FILL_ARGS_RD_ONLY    )
+    INSTRUCTION ( LPM   , ((cmd & 0xFE0F) == 0x9004) ,     3    , FILL_ARGS_Rd_ONLY    )
 
     INSTRUCTION ( RCALL , ((cmd & 0xF000) == 0xD000) ,     3    , FILL_ARGS_RJMP_RCALL )
     INSTRUCTION ( RJMP  , ((cmd & 0xF000) == 0xC000) ,     2    , FILL_ARGS_RJMP_RCALL )
@@ -116,8 +116,8 @@ do {                                                             \
     INSTRUCTION ( BRID  , ((cmd & 0xFC07) == 0xF407) ,     2    , FILL_ARGS_BRANCH     )
 
 
-    INSTRUCTION ( CPI   , ((cmd & 0xF000) == 0x3000) ,     1    , FILL_ARGS_LDI_CPI    )
-    INSTRUCTION ( LDI   , ((cmd & 0xF000) == 0xE000) ,     1    , FILL_ARGS_LDI_CPI    )
+    INSTRUCTION ( CPI   , ((cmd & 0xF000) == 0x3000) ,     1    , FILL_ARGS_Rd_K       )
+    INSTRUCTION ( LDI   , ((cmd & 0xF000) == 0xE000) ,     1    , FILL_ARGS_Rd_K       )
 
     INSTRUCTION ( NOP   , ((cmd & 0xFFFF) == 0x0000) ,     1    , FILL_ARGS_NO_ARGS    )
     INSTRUCTION ( RET   , ((cmd & 0xFFFF) == 0x9508) ,     4    , FILL_ARGS_NO_ARGS    )
@@ -129,10 +129,10 @@ do {                                                             \
 #undef FILL_ARGS_IN_OUT
 #undef FILL_ARGS_CBI_SBI
 #undef FILL_ARGS_ALU
-#undef FILL_ARGS_RD_ONLY
+#undef FILL_ARGS_Rd_ONLY
 #undef FILL_ARGS_BRANCH
 #undef FILL_ARGS_RJMP_RCALL
-#undef FILL_ARGS_LDI_CPI
-#undef FILL_ARGS_LONG_D
+#undef FILL_ARGS_Rd_K
+#undef FILL_ARGS_LONG_Rd
 #undef FILL_ARGS_SER
 #undef FILL_ARGS_NO_ARGS
